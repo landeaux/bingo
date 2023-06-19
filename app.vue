922c3d9062e7dd9e66f74f1a78741d889eb38821
lando <pluckedowl@gmail.com>
2023-06-19 16:30:31 -0700

    add simple form to submit a genre and generate names

--- app.vue
+++ app.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
 const title = ref("Bingo");
 useHead({ title });
+
+const genreInput = ref("");
+const result = ref("");
+
+function onSubmit() {
+  genreInput.value = "";
+  result.value = "The Doors, The Beatles, The Rolling Stones";
+}
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <main>
+    <h1>{{ title }}</h1>
+    <h2>Generate Band Names</h2>
+    <form @submit.prevent="onSubmit">
+      <label for="genre">Genre:</label>
+      <input
+        id="genre"
+        v-model="genreInput"
+        type="text"
+        name="genre"
+        placeholder="Enter a genre"
+      />
+      <input type="submit" value="Generate names" />
+    </form>
+    <div>{{ result }}</div>
+  </main>
 </template>
--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,7 +1,21 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("/");
+test.describe("App", () => {
+  test("has title", async ({ page }) => {
+    await page.goto("/");
 
-  await expect(page).toHaveTitle(/Bingo/);
+    await expect(page).toHaveTitle(/Bingo/);
+  });
+
+  test("can generate band names", async ({ page }) => {
+    await page.goto("/");
+
+    await expect(page.getByText(/.+, .+, .+/)).not.toBeVisible();
+
+    await page.getByLabel("Genre").click();
+    await page.getByLabel("Genre").fill("Indie Rock");
+    await page.getByRole("button", { name: "Generate names" }).click();
+
+    await expect(page.getByText(/.+, .+, .+/)).toBeVisible();
+  });
 });
